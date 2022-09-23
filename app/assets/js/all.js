f74d0777c83bff5dae7175e113961366b1ffe981
CodingFisher <cppmaster@outlook.com>
2022-09-23 15:38:22 +0930

    test

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,3 +2,13 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 
+
+const resizeObserver = new ResizeObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry.contentRect.height)
+      const heightChanger = document.querySelector('#navbar-filler')
+      console.log(heightChanger);
+      heightChanger.style.height = entry.contentRect.height + 3 + 'px'
+    })
+  })
+  resizeObserver.observe(document.querySelector('#barWatcher'))
