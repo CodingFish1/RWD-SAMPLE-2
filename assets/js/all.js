cf19f2d5fa157b7592b3806ecdf322ce02df950d
CodingFisher <cppmaster@outlook.com>
2022-09-23 15:37:53 +0930

    Update 2022-09-23T06:07:50.339Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,6 +3,15 @@
 $(function () {
   console.log('Hello Bootstrap5');
 });
+var resizeObserver = new ResizeObserver(function (entries) {
+  entries.forEach(function (entry) {
+    console.log(entry.contentRect.height);
+    var heightChanger = document.querySelector('#navbar-filler');
+    console.log(heightChanger);
+    heightChanger.style.height = entry.contentRect.height + 3 + 'px';
+  });
+});
+resizeObserver.observe(document.querySelector('#barWatcher'));
 "use strict";
 
 var swiper = new Swiper('.swiper', {
