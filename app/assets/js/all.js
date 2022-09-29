62b2080bc37527ca0bbe1f645037fa36faad4719
CodingFisher <cppmaster@outlook.com>
2022-09-29 14:39:57 +0930

    fun

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,14 +1,69 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
-
+let headerHeight = null;
 
 const resizeObserver = new ResizeObserver((entries) => {
   entries.forEach((entry) => {
-      console.log(entry.contentRect.height)
       const heightChanger = document.querySelector('#navbar-filler')
-      console.log(heightChanger);
+      headerHeight = '-' + entry.contentRect.height + 3
       heightChanger.style.height = entry.contentRect.height + 3 + 'px'
+      console.log(headerHeight);
     })
   })
   resizeObserver.observe(document.querySelector('#barWatcher'))
+
+  // const header = document.querySelector("header");
+  // const section =["One","Two","Three","Four","Five","Six","Seven","Eight"]
+  // section.forEach(item=>{
+  //   const section + item = 
+  // })
+  // const sectionOne = document.querySelector("#sectionOne");
+  // const sectionTwo = document.querySelector("#sectionTwo");
+  // const sectionThree = document.querySelector("#sectionThree");
+  // const sectionFour = document.querySelector("#sectionFour");
+  // const sectionFive = document.querySelector("#sectionFive");
+  // const sectionSix = document.querySelector("#sectionSix");
+  // const sectionSeven= document.querySelector("#sectionSeven");
+  // const sectionEight = document.querySelector("#sectionEight");
+    const sections = [...document.querySelectorAll("section")]
+    console.log(sections);
+    sections.forEach((item) => {
+      // const item.id = document.querySelector(`'#'+${item}`)
+      console.log(item.id);
+    })
+  
+  const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+    // rootMargin: `${headerHeight}px`
+  };
+  
+  // const sectionObserverCallback = new IntersectionObserver(function(
+  //   entries) {
+  //   entries.forEach(entry => {
+  //     if(entry.target.attributes[1].nodeValue === 'sectionOne' && !entry.isIntersecting){
+  //       console.log('sectionOne');
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionTwo' && !entry.isIntersecting){
+  //       console.log('sectionTwo');
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionThree' && !entry.isIntersecting){
+  //       console.log('sectionThree');
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionFour' && !entry.isIntersecting){
+  //       console.log('sectionFour');
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionFive' && !entry.isIntersecting){
+  //       console.log('sectionFive');
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionSix' && !entry.isIntersecting){
+  //       console.log('sectionSix');
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionSeven' && !entry.isIntersecting){
+  //       console.log('sectionSeven')
+  //     } else if(entry.target.attributes[1].nodeValue === 'sectionEight' && !entry.isIntersecting){
+  //       console.log('sectionEight');
+  //     }
+  //   });
+  // },
+  // sectionOneOptions);
+  
+  // sectionObserverCallback.observe(sectionOne);
+  // sectionObserverCallback.observe(sectionTwo);
+  // sectionObserverCallback.observe(sectionThree);
+  // sectionObserverCallback.observe(sectionFour);
+  // sectionObserverCallback.observe(sectionFive);
+  // sectionObserverCallback.observe(sectionSix);
+  // sectionObserverCallback.observe(sectionSeven);
+  // sectionObserverCallback.observe(sectionEight);
